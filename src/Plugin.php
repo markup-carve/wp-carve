0e49c9969e0b5658fddfe408bc507a7d9f0e666e
Mark Scherer <dereuromark@users.noreply.github.com>
2026-07-12 23:11:35 +0200

    Fix control-structure line break (phpcbf)

--- src/Plugin.php
+++ src/Plugin.php
@@ -144,7 +144,8 @@ class Plugin
         $codeFence = null;
         foreach ($lines as $i => $line) {
             if ($codeFence !== null) {
-                if (preg_match('/^[ \t]*(`{3,}|~{3,})[ \t]*$/', $line, $m) === 1
+                if (
+                    preg_match('/^[ \t]*(`{3,}|~{3,})[ \t]*$/', $line, $m) === 1
                     && $m[1][0] === $codeFence[0]
                     && strlen($m[1]) >= strlen($codeFence)
                 ) {
